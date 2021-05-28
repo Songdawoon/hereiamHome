e4cb3277edbc4c1f6e125d8989e7389b263ad061
Songwoonda <songwoonda@knou.ac.kr>
2021-05-28 14:46:02 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// 헤더 제어
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click',function(){
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener ('focus',function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-})
-searchInputEl.addEventListener ('blur',function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-})
-
 // 스크롤 제어
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -134,8 +118,4 @@ spyEls.forEach(function(spyEl){
   .setClassToggle(spyEl, 'show')
   .addTo(new ScrollMagic.Controller());
 })
-
-// footer display year
-let thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
 
